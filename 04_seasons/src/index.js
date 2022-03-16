9df5772eb5bf23c6c9ec4c31279fb0fd0dc66583
Weiyi Xu <johnsonxwy@gmail.com>
2022-03-16 23:07:21 +0800

    feat: add geolocation

--- 04_seasons/src/index.js
+++ 04_seasons/src/index.js
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log('position: ', position),
+    err => console.log('err: ', err)
+  )
+
   return (
     <div>Hi there!</div>
   )
