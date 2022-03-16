ffaa10205d5db739195b9528e152b6f1e77a0cab
Weiyi Xu <johnsonxwy@gmail.com>
2022-03-16 22:41:44 +0800

    feat: use prop.children

--- 03_components/src/index.js
+++ 03_components/src/index.js
@@ -10,10 +10,17 @@ import './index.css'
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCart />
-      <CommentDetail author="Sam" timeAgo="Today at 4:00PM" comment="Nice blog post!" avatarSrc={faker.image.avatar()} />
-      <CommentDetail author="Alex" timeAgo="Today at 11:30AM" comment="Great job!" avatarSrc={faker.image.avatar()} />
-      <CommentDetail author="Jane" timeAgo="Yesterday at 7:15PM" comment="Keep it going!" avatarSrc={faker.image.avatar()} />
+      <ApprovalCart>
+        <CommentDetail author="Sam" timeAgo="Today at 4:00PM" comment="Nice blog post!" avatarSrc={faker.image.avatar()} />
+      </ApprovalCart>
+
+      <ApprovalCart>
+        <CommentDetail author="Alex" timeAgo="Today at 11:30AM" comment="Great job!" avatarSrc={faker.image.avatar()} />
+      </ApprovalCart>
+
+      <ApprovalCart>
+        <CommentDetail author="Jane" timeAgo="Yesterday at 7:15PM" comment="Keep it going!" avatarSrc={faker.image.avatar()} />
+      </ApprovalCart>
     </div>
   )
 }
