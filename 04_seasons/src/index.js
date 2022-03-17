09d10ac881159db2e30cf75797a7dc3a2aa4ce9f
Weiyi Xu <johnsonxwy@gmail.com>
2022-03-17 13:16:49 +0800

    feat: use state to update latitude

--- 04_seasons/src/index.js
+++ 04_seasons/src/index.js
@@ -2,14 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-  render() {
+  // state 需要在最开始进行初始化，所以放在 constructor 里
+  constructor(props) {
+    // 由于是继承关系，所以要先调用父组件的 constructor
+    super(props)
+
+    // THIS IS THE ONLY TIME we directly assign this.state
+    this.state = { lat: null }
+
+    // 由于每次 state 变化都会导致 rerender
+    // 所以像获取位置这种请求我们不想放在 render() 函数里
+    // 放在 constructor 里会在 App 初始化时就调用，且只会调用一次
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log('success: ', position),
+      position => this.setState({ lat: position.coords.latitude }),
       err => console.log('error: ', err)
     )
+  }
 
+  render() {
     return (
-      <div>Latitude: </div>
+      <div>Latitude: {this.state.lat}</div>
     )
   }
 }
