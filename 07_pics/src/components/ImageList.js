0406376edc03dbe44b4d11f6268fa9823995dc72
Weiyi Xu <johnsonxwy@gmail.com>
2022-03-20 16:36:20 +0800

    feat: add css to image list

--- 07_pics/src/components/ImageList.js
+++ 07_pics/src/components/ImageList.js
@@ -1,11 +1,17 @@
+import './ImageList.css'
 import React from 'react'
 
 const ImageList = ({ images }) => {
   const imageList = images.map(({ id, description, urls }) => (
-    <img key={id} src={urls.regular} alt={description} />
+    <img
+      key={id}
+      src={urls.regular}
+      alt={description}
+      className="image-list__img"
+    />
   ))
 
-  return <div>{imageList}</div>
+  return <div className="image-list">{imageList}</div>
 }
 
 export default ImageList
