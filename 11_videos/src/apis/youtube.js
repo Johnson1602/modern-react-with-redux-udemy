0f3db329bb4d036929414b52ccc8ea4ed205beda
Weiyi Xu <johnsonxwy@gmail.com>
2022-03-23 13:17:20 +0800

    feat: add search api for youtube videos

---
+++ 11_videos/src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const KEY = 'AIzaSyBVCreb5fPkWmHWetgYr7S_N9oInipCVF0'
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    key: KEY,
+    part: 'snippet',
+    maxResults: 5,
+  },
+})
--- 11_videos/src/components/SearchBar.js
+++ 11_videos/src/components/SearchBar.js
@@ -1,8 +1,36 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+  state = {
+    term: '',
+  }
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value })
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault()
+
+    this.props.onTermSubmit(this.state.term)
+  }
+
   render() {
-    return <div>SearchBar</div>
+    return (
+      <div className="ui segment search-bar">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="ui field">
+            <label htmlFor="video-search-input">Video Search</label>
+            <input
+              type="text"
+              id="video-search-input"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    )
   }
 }
 
