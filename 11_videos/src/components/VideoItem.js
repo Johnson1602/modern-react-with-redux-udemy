37c24de40b4a52a7bab129c41a72e35a70655bbe
Weiyi Xu <johnsonxwy@gmail.com>
2022-03-25 13:20:52 +0800

    fix: add img alt

--- 11_videos/src/components/VideoItem.js
+++ 11_videos/src/components/VideoItem.js
@@ -3,7 +3,10 @@ import React from 'react'
 const VideoItem = ({ video }) => {
   return (
     <div>
-      <img src={video.snippet.thumbnails.medium.url} />
+      <img
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}
+      />
       <span>{video.snippet.title}</span>
     </div>
   )
