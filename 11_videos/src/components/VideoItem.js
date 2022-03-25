057729e95efeb7ee0d0a26e59eb5c980eef17d67
Weiyi Xu <johnsonxwy@gmail.com>
2022-03-25 13:19:34 +0800

    feat: show thumbnails and titles

---
+++ 11_videos/src/components/VideoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const VideoItem = ({ video }) => {
+  return (
+    <div>
+      <img src={video.snippet.thumbnails.medium.url} />
+      <span>{video.snippet.title}</span>
+    </div>
+  )
+}
+
+export default VideoItem
