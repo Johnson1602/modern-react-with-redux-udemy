5401becc7dfacb71f9bd257e10ff6c15f09ff8d9
Weiyi Xu <johnsonxwy@gmail.com>
2022-03-30 12:47:09 +0800

    feat: add search to wikipedia

--- 12_widgets/src/components/Search.js
+++ 12_widgets/src/components/Search.js
@@ -1,12 +1,32 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Search = () => {
   const [term, setTerm] = useState('')
+  const [results, setResults] = useState([])
 
   const onTermChange = (e) => {
     setTerm(e.target.value)
   }
+
+  useEffect(() => {
+    const search = async () => {
+      const result = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      })
+
+      setResults(result.data.query.search)
+    }
+
+    !!term && search()
+  }, [term])
 
   return (
     <div>
