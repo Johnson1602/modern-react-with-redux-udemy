707e55cbdd9b4ceb031a8d5cf1afbfd954514801
Weiyi Xu <johnsonxwy@gmail.com>
2022-04-24 13:23:00 +0800

    feat: add reducer

---
+++ 17_songs/src/reducers/index.js
@@ -0,0 +1,32 @@
+const songsReducer = () => {
+  return [
+    {
+      title: 'Love Letter',
+      duration: '4:13',
+    },
+    {
+      title: 'Easy Come Easy Go',
+      duration: '3:00',
+    },
+    {
+      title: 'Less Than Zero',
+      duration: '3:32',
+    },
+    {
+      title: 'Better Days',
+      duration: '3:14',
+    },
+    {
+      title: 'Let Somebody Go',
+      duration: '4:02',
+    },
+  ]
+}
+
+const selectSongReducer = (currentSelectedSong = null, action) => {
+  if (action.type === 'SELECT_SONG') {
+    return action.payload
+  }
+
+  return currentSelectedSong
+}
